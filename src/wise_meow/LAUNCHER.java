ba8550ea6b10bf81fe7be429dc1c9dc205e59c35
shuhanmirza <shuhan.mirza@gmail.com>
2017-02-03 20:44:09 +0600

    bismillah

---
+++ src/wise_meow/LAUNCHER.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wise_meow;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import wise.meow.display.DisplayEasy;
+
+/**
+ *
+ * @author shuhan
+ */
+public class LAUNCHER {
+
+//    public static void main(String[] args) {
+//
+//
+//        String res;
+//        String[] temp = new String[100];
+//        String str;
+//        
+//        int i, l, k;
+//
+//        WiseMeow kevin = new WiseMeow();
+//
+//        System.out.println("Hi , I am Kevin.....I am a Wise Meow of the MeowLand. What is your name? ");
+//
+//        Scanner s1 = new Scanner(System.in);
+//
+//        while (true) {
+//            
+//            //test
+//            
+//            
+//            //test ends
+//            
+//
+//            System.out.println("you: ");//get the query to search
+//
+//            str = s1.nextLine();
+//
+//            System.out.print("MEOW: ");
+//
+//            str = str.toLowerCase();
+//            if (str.contains("bye") == true && str.endsWith("bye") && str.startsWith("good")) {
+//                System.out.println("Good Bye :D Have a nice day :D");
+//                break;
+//            }
+//
+//            res = kevin.chat(str);
+//            
+//            System.out.println(res);
+//
+//            l = res.length();
+//  
+//             //it is time to use jsoup
+//            //System.out.println(res.length());
+//
+//        }
+//
+//    }
+}
